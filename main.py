de06741da7e7cc20b8387b4e37f9d18a41b45230
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-04-04 09:18:30 -0300

    Camera video working

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import time
+
 import cv2
 
 video = cv2.VideoCapture(0)
-check, frame = video.read()
+time.sleep(1)
 
+while True:
+    check, frame = video.read()
+    cv2.imshow("Camera", frame)
+
+    key = cv2.waitKey(1)
+
+
+
+    if key == ord("q"):
+        break
+
+video.release()
